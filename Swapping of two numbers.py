d7a46c53ecee7138f94a575bf4e40f9ba7fae355
javahark67 <javahark676@gmail.com>
2026-04-15 22:14:01 +0530

    Add files via upload

---
+++ Swapping of two numbers.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the First element :"))
+b = int(input("Enter the Second element :"))
+
+print("Before swapping:")
+print("a=",a)
+print("b=",b)
+
+a,b=b,a
+
+print("Value after swapping:")
+print("a=",a)
+print("b=",b)
